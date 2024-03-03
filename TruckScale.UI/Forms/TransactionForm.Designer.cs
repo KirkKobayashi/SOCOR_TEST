6ab16163ac52537382fec8af82b14330b0abded0
KirkKobayashi <50836881+KirkKobayashi@users.noreply.github.com>
2024-03-03 11:30:03 +0800

    Finished designing new transaction form

---
+++ TruckScale.UI/Forms/TransactionForm.Designer.cs
@@ -0,0 +1,460 @@
+﻿namespace TruckScale.UI.Forms
+{
+    partial class TransactionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TransactionForm));
+            panel1 = new Panel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            panel5 = new Panel();
+            panel3 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            txtPlateNumber = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            txtSupplier = new TextBox();
+            txtCustomer = new TextBox();
+            txtItem = new TextBox();
+            txtQuantity = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            txtFirstWeight = new TextBox();
+            txtSecondWeight = new TextBox();
+            txtNetWeight = new TextBox();
+            lblWeighIn = new Label();
+            lblWeighOut = new Label();
+            btnGetWeight = new Button();
+            label12 = new Label();
+            txtDriver = new TextBox();
+            btnSave = new Button();
+            btnExit = new Button();
+            btnPrint = new Button();
+            txtTicket = new TextBox();
+            panel2 = new Panel();
+            panel4 = new Panel();
+            panel1.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            panel1.Controls.Add(tableLayoutPanel1);
+            panel1.Location = new Point(8, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(407, 525);
+            panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33333F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333359F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333359F));
+            tableLayoutPanel1.Controls.Add(panel5, 0, 16);
+            tableLayoutPanel1.Controls.Add(panel3, 0, 2);
+            tableLayoutPanel1.Controls.Add(label1, 0, 1);
+            tableLayoutPanel1.Controls.Add(label2, 0, 3);
+            tableLayoutPanel1.Controls.Add(txtPlateNumber, 1, 3);
+            tableLayoutPanel1.Controls.Add(label3, 0, 4);
+            tableLayoutPanel1.Controls.Add(label4, 0, 5);
+            tableLayoutPanel1.Controls.Add(label5, 0, 6);
+            tableLayoutPanel1.Controls.Add(txtSupplier, 1, 4);
+            tableLayoutPanel1.Controls.Add(txtCustomer, 1, 5);
+            tableLayoutPanel1.Controls.Add(txtItem, 1, 6);
+            tableLayoutPanel1.Controls.Add(txtQuantity, 1, 7);
+            tableLayoutPanel1.Controls.Add(label6, 0, 7);
+            tableLayoutPanel1.Controls.Add(label7, 0, 10);
+            tableLayoutPanel1.Controls.Add(label8, 0, 11);
+            tableLayoutPanel1.Controls.Add(label9, 0, 12);
+            tableLayoutPanel1.Controls.Add(txtFirstWeight, 1, 10);
+            tableLayoutPanel1.Controls.Add(txtSecondWeight, 1, 11);
+            tableLayoutPanel1.Controls.Add(txtNetWeight, 1, 12);
+            tableLayoutPanel1.Controls.Add(lblWeighIn, 2, 10);
+            tableLayoutPanel1.Controls.Add(lblWeighOut, 2, 11);
+            tableLayoutPanel1.Controls.Add(btnGetWeight, 2, 12);
+            tableLayoutPanel1.Controls.Add(label12, 0, 8);
+            tableLayoutPanel1.Controls.Add(txtDriver, 1, 8);
+            tableLayoutPanel1.Controls.Add(btnSave, 2, 14);
+            tableLayoutPanel1.Controls.Add(btnExit, 2, 15);
+            tableLayoutPanel1.Controls.Add(btnPrint, 0, 14);
+            tableLayoutPanel1.Controls.Add(txtTicket, 1, 1);
+            tableLayoutPanel1.Controls.Add(panel2, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel4, 0, 9);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 17;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 10F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 45F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 45F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.Size = new Size(407, 525);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(170, 217, 34);
+            tableLayoutPanel1.SetColumnSpan(panel5, 3);
+            panel5.Location = new Point(3, 483);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(401, 2);
+            panel5.TabIndex = 29;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(170, 217, 34);
+            tableLayoutPanel1.SetColumnSpan(panel3, 3);
+            panel3.Location = new Point(3, 43);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(401, 2);
+            panel3.TabIndex = 27;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 13);
+            label1.TabIndex = 0;
+            label1.Text = "Weighing Slip No.";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 60);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 13);
+            label2.TabIndex = 2;
+            label2.Text = "Plate Number";
+            // 
+            // txtPlateNumber
+            // 
+            txtPlateNumber.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.SetColumnSpan(txtPlateNumber, 2);
+            txtPlateNumber.Location = new Point(138, 63);
+            txtPlateNumber.Name = "txtPlateNumber";
+            txtPlateNumber.Size = new Size(266, 22);
+            txtPlateNumber.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 90);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 13);
+            label3.TabIndex = 3;
+            label3.Text = "Supplier";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 120);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 13);
+            label4.TabIndex = 4;
+            label4.Text = "Customer";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 150);
+            label5.Name = "label5";
+            label5.Size = new Size(29, 13);
+            label5.TabIndex = 5;
+            label5.Text = "Item";
+            // 
+            // txtSupplier
+            // 
+            txtSupplier.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.SetColumnSpan(txtSupplier, 2);
+            txtSupplier.Location = new Point(138, 93);
+            txtSupplier.Name = "txtSupplier";
+            txtSupplier.Size = new Size(266, 22);
+            txtSupplier.TabIndex = 7;
+            // 
+            // txtCustomer
+            // 
+            txtCustomer.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.SetColumnSpan(txtCustomer, 2);
+            txtCustomer.Location = new Point(138, 123);
+            txtCustomer.Name = "txtCustomer";
+            txtCustomer.Size = new Size(266, 22);
+            txtCustomer.TabIndex = 8;
+            // 
+            // txtItem
+            // 
+            txtItem.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.SetColumnSpan(txtItem, 2);
+            txtItem.Location = new Point(138, 153);
+            txtItem.Name = "txtItem";
+            txtItem.Size = new Size(266, 22);
+            txtItem.TabIndex = 9;
+            // 
+            // txtQuantity
+            // 
+            txtQuantity.Location = new Point(138, 183);
+            txtQuantity.Name = "txtQuantity";
+            txtQuantity.Size = new Size(127, 22);
+            txtQuantity.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(3, 180);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 13);
+            label6.TabIndex = 11;
+            label6.Text = "Quantity";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(3, 270);
+            label7.Name = "label7";
+            label7.Size = new Size(70, 13);
+            label7.TabIndex = 12;
+            label7.Text = "First Weight";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(3, 300);
+            label8.Name = "label8";
+            label8.Size = new Size(86, 13);
+            label8.TabIndex = 13;
+            label8.Text = "Second Weight";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(3, 330);
+            label9.Name = "label9";
+            label9.Size = new Size(66, 13);
+            label9.TabIndex = 14;
+            label9.Text = "Net Weight";
+            // 
+            // txtFirstWeight
+            // 
+            txtFirstWeight.BackColor = Color.White;
+            txtFirstWeight.Location = new Point(138, 273);
+            txtFirstWeight.Name = "txtFirstWeight";
+            txtFirstWeight.ReadOnly = true;
+            txtFirstWeight.Size = new Size(127, 22);
+            txtFirstWeight.TabIndex = 15;
+            txtFirstWeight.TabStop = false;
+            // 
+            // txtSecondWeight
+            // 
+            txtSecondWeight.BackColor = Color.White;
+            txtSecondWeight.Location = new Point(138, 303);
+            txtSecondWeight.Name = "txtSecondWeight";
+            txtSecondWeight.ReadOnly = true;
+            txtSecondWeight.Size = new Size(127, 22);
+            txtSecondWeight.TabIndex = 16;
+            txtSecondWeight.TabStop = false;
+            // 
+            // txtNetWeight
+            // 
+            txtNetWeight.BackColor = Color.White;
+            txtNetWeight.Location = new Point(138, 333);
+            txtNetWeight.Name = "txtNetWeight";
+            txtNetWeight.ReadOnly = true;
+            txtNetWeight.Size = new Size(127, 22);
+            txtNetWeight.TabIndex = 17;
+            txtNetWeight.TabStop = false;
+            // 
+            // lblWeighIn
+            // 
+            lblWeighIn.AutoSize = true;
+            lblWeighIn.Location = new Point(273, 270);
+            lblWeighIn.Name = "lblWeighIn";
+            lblWeighIn.Size = new Size(0, 13);
+            lblWeighIn.TabIndex = 18;
+            // 
+            // lblWeighOut
+            // 
+            lblWeighOut.AutoSize = true;
+            lblWeighOut.Location = new Point(273, 300);
+            lblWeighOut.Name = "lblWeighOut";
+            lblWeighOut.Size = new Size(0, 13);
+            lblWeighOut.TabIndex = 19;
+            // 
+            // btnGetWeight
+            // 
+            btnGetWeight.Location = new Point(273, 333);
+            btnGetWeight.Name = "btnGetWeight";
+            btnGetWeight.Size = new Size(128, 23);
+            btnGetWeight.TabIndex = 20;
+            btnGetWeight.Text = "Update Weight";
+            btnGetWeight.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(3, 210);
+            label12.Name = "label12";
+            label12.Size = new Size(37, 13);
+            label12.TabIndex = 21;
+            label12.Text = "Driver";
+            // 
+            // txtDriver
+            // 
+            txtDriver.Location = new Point(138, 213);
+            txtDriver.Name = "txtDriver";
+            txtDriver.Size = new Size(127, 22);
+            txtDriver.TabIndex = 22;
+            // 
+            // btnSave
+            // 
+            btnSave.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnSave.Location = new Point(311, 393);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(93, 39);
+            btnSave.TabIndex = 23;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnExit.Location = new Point(311, 438);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(93, 39);
+            btnExit.TabIndex = 24;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnPrint
+            // 
+            btnPrint.Location = new Point(3, 393);
+            btnPrint.Name = "btnPrint";
+            btnPrint.Size = new Size(96, 39);
+            btnPrint.TabIndex = 25;
+            btnPrint.Text = "Print";
+            btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // txtTicket
+            // 
+            txtTicket.Location = new Point(138, 13);
+            txtTicket.Name = "txtTicket";
+            txtTicket.Size = new Size(127, 22);
+            txtTicket.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(63, 136, 197);
+            tableLayoutPanel1.SetColumnSpan(panel2, 3);
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(401, 2);
+            panel2.TabIndex = 26;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(170, 217, 34);
+            tableLayoutPanel1.SetColumnSpan(panel4, 3);
+            panel4.Location = new Point(3, 243);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(401, 2);
+            panel4.TabIndex = 28;
+            // 
+            // TransactionForm
+            // 
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(422, 527);
+            Controls.Add(panel1);
+            Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            MinimumSize = new Size(438, 500);
+            Name = "TransactionForm";
+            Text = "WEIGHING TRANSACTION";
+            Load += TransactionForm_Load;
+            panel1.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
+        private TextBox txtTicket;
+        private Label label2;
+        private TextBox txtPlateNumber;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txtSupplier;
+        private TextBox txtCustomer;
+        private TextBox txtItem;
+        private TextBox txtQuantity;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox txtFirstWeight;
+        private TextBox txtSecondWeight;
+        private TextBox txtNetWeight;
+        private Label lblWeighIn;
+        private Label lblWeighOut;
+        private Button btnGetWeight;
+        private Label label12;
+        private TextBox txtDriver;
+        private Button btnSave;
+        private Button btnExit;
+        private Button btnPrint;
+        private Panel panel2;
+        private Panel panel4;
+        private Panel panel3;
+        private Panel panel5;
+    }
+}
